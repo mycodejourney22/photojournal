d83154d50e4c52b7817aedbf891d978982399895
mycodejourney22 <oloyedeafeez@363photography.org>
2024-06-23 12:10:03 +0100

    added policy for sale

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,14 +1,19 @@
 class SalesController < ApplicationController
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
   def index
-    @sales = Sale.all
+    authorize Sale
+    @sales = policy_scope(Sale).all.order(created_at: :desc)
   end
 
   def new
+    authorize Sale
     @sale = Sale.new
   end
 
 
   def create
+    authorize Sale
     @sale = Sale.new(sale_params)
     @sale.location = determine_location(current_user)
 
@@ -20,6 +25,7 @@ class SalesController < ApplicationController
   end
 
   def upfront
+    authorize Sale
     @sales = Sale.where(payment_type: "Part payment")
     respond_to do |format|
       format.html # Follow regular flow of Rails
