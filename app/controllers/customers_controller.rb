90b10ba74c30401be70a1e76bff983d07758f46b
mycodejourney22 <oloyedeafeez@363photography.org>
2024-08-26 17:06:33 +0100

    added customer show

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -8,4 +8,12 @@ class CustomersController < ApplicationController
       format.text { render partial: "table", locals: {customers: @customers}, formats: [:html] }
     end
   end
+
+  def show
+    @customer = Customer.find(params[:id])
+    phone_number = @customer.phone_number
+    @appointments = Appointment.joins(:questions)
+                               .where(questions: { question: 'Phone number' })
+                               .where("REGEXP_REPLACE(questions.answer, '\\D', '', 'g') = ?", phone_number)
+  end
 end
