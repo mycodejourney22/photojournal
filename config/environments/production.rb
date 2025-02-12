3d1a71096a0b12d22fdce4cae1627ccf0e8b60cc
mycodejourney22 <oloyedeafeez@363photography.org>
2025-02-12 01:00:13 +0000

    changed image bucket to cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :wasabi
+  config.active_storage.service = :cloudinary
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'https://photologger-0d07f7db019d.herokuapp.com/' }
 
