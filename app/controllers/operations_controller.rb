96bc026c1f4843a37c103933c0e8db015e73550d
mycodejourney22 <oloyedeafeez@363photography.org>
2025-03-16 17:42:19 +0000

    modified date

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,13 +1,19 @@
 class OperationsController < ApplicationController
 
   def index
+    # Use a proper IANA timezone name for your region
+    timezone = "Africa/Lagos"  # This is the timezone for West Central Africa
+
+    date_sql = "DATE(date AT TIME ZONE 'UTC' AT TIME ZONE '#{timezone}')"
+
     @sales = policy_scope(Sale)
       .where(void: false)
       .where(date: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
-      .group("DATE(date)")
-      .order("DATE(date) DESC")
+      .group(Arel.sql(date_sql))
+      .order(Arel.sql("#{date_sql} DESC"))
       .sum(:amount_paid)
-      @total_sales = @sales.values.sum
+
+    @total_sales = @sales.values.sum
   end
 
   def daily_sales
